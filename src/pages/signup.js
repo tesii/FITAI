20ddb268697af2a4eaaa005d1bbe1a39df98e862
patience kabatesi <130172829+tesii@users.noreply.github.com>
2026-05-31 00:36:12 +0200

    Update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
+const supabase = supabase.createClient(
   "https://ojzeaqememaevlxcyabn.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qemVhcWVtZW1hZXZseGN5YWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3NTQxODIsImV4cCI6MjA4OTMzMDE4Mn0.4Dg17eTLAK0AlHXmrCCvxEIa2RngJIu0kr1v5rX439Y"
 );
